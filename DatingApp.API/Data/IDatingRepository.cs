e74b14f18840a83375eacfb542b945584574b780
koniia <hulusikafalier@gmail.com>
2019-06-25 16:06:46 +0300

    Created dating methods.

---
+++ DatingApp.API/Data/IDatingRepository.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Data
+{
+    public interface IDatingRepository
+    {
+        // T becomes given type(Photo, User)
+         void Add<T>(T entity) where T: class;
+         void Delete<T>(T entity) where T: class;
+         Task<bool> SaveAll();
+         Task<IEnumerable<User>> GetUsers();
+         Task<User> GetUser(int id);
+    }
+}
